221e1a8e931aa746e2f5d154fc8a83f3f40a6fdb
Toni Hansen <ton1ght@protonmail.com>
2023-03-04 16:10:20 +0100

    Disabled session restore after crash

--- .mozilla/firefox/user-overrides.js
+++ .mozilla/firefox/user-overrides.js
@@ -19,3 +19,4 @@ user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.bookmark", false);
 user_pref("browser.urlbar.suggest.openpage", false);
 user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.sessionstore.resume_from_crash", false);
