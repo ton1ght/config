751050cf71a7134ac3bb9f906b26d732d4f62759
ton1ght <ton1ght@pm.me>
2023-05-12 11:18:20 +0200

    fehbg now sets background according to hostname

--- scripts/fehbg.sh
+++ scripts/fehbg.sh
@@ -1,2 +1,6 @@
-feh --bg-fill ~/pictures/wallpaper/catppuccin.png --bg-fill ~/pictures/wallpaper/solid-grey.jpeg
-
+if [[ $HOST -eq "kekpc" ]]
+then
+    feh --bg-scale ~/pictures/wallpaper/catppuccin.jpg
+else
+    feh --bg-fill ~/pictures/wallpaper/catppuccin.png --bg-fill ~/pictures/wallpaper/solid-grey.jpeg
+fi
