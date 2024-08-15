d5f60a5e939822bd1399d6315105f2ad55ad2471
ton1ght <ton1ght@protonmail.com>
2024-08-15 14:22:23 +0200

    nvim: telescope: use ctrl-j and ctrl-k for down/up

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,9 +4,24 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
+        local actions = require('telescope.actions')
+
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Buffers" })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help Tags" })
+
+        require("telescope").setup({
+            defaults = {
+                path_display = { "smart" },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next, -- move to next result
+                    },
+                },
+            },
+        })
+
     end,
 }
