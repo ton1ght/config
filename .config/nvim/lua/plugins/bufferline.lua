a99ddd2a6be330ee66ec0d32a913300119d76af3
ton1ght <ton1ght@protonmail.com>
2024-08-15 14:21:59 +0200

    nvim: offset bufferline when nvim-tree is active

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,7 +4,19 @@ return {
         version = "*",
         dependencies = 'nvim-tree/nvim-web-devicons',
         config = function ()
-            require("bufferline").setup()
+            require("bufferline").setup({
+                options = {
+                    separator_style = "slant",
+                    offsets = {
+                        {
+                            filetype = "NvimTree",
+                            text = "File Explorer",
+                            highlight = "Directory",
+                            separator = true -- use a "true" to enable the default, or set your own character
+                        }
+                    }
+                }
+            })
         end
     }
 }
