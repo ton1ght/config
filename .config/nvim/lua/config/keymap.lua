e0fbc41237eb5aad5f086c225627839abea1f246
ton1ght <ton1ght@protonmail.com>
2024-08-15 14:21:45 +0200

    nvim: better window/split management

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,7 +1,17 @@
 -- set leader to space
 vim.g.mapleader = " "
 
+-- better window movement
 vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
 vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
 vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
 vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+
+-- buffer management
+vim.keymap.set("n", "<leader>bc", "<cmd>bd<cr>", { desc = "Close buffer" })
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
