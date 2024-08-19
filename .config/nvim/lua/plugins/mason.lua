b1261a726547a5f630ca640f365cc763def56089
ton1ght <ton1ght@protonmail.com>
2024-08-19 13:10:10 +0200

    nvim: use mason-tool-installer

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,19 +1,47 @@
 return {
-    { "williamboman/mason.nvim",
+    {
+        "williamboman/mason.nvim",
         config = function()
-            require("mason").setup()
-        end,
-    },
-    { "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "pyright",
-                    "clangd",
-                    "bashls",
-                    "lua_ls",
-                }
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
             })
         end,
     },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason-lspconfig").setup()
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                "pyright", -- python lsp
+                "clangd", -- c/cpp lsp
+                "bashls", -- bash lsp
+                "lua_ls", -- lua lsp
+
+                "shellcheck", -- bash linter
+
+                "black", -- python formatter
+                "stylua", -- lua formatter
+                "shfmt", -- sh formatter
+                }
+            })
+        end,
+    }
 }
