e29ea9c2e303aa86e99bbfa7fb3a0ebfbdc36793
ton1ght <ton1ght@protonmail.com>
2024-08-19 13:11:12 +0200

    nvim: use <leader>gr to show references

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
 
                 -- set keybinds
                 opts.desc = "Show LSP references"
-                vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+                vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
                 opts.desc = "Go to declaration"
                 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
