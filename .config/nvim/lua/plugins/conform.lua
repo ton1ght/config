b8713ffd46f83ffc8fce48e8d394acb2eb9bfc89
ton1ght <ton1ght@protonmail.com>
2024-08-19 14:14:05 +0200

    nvim: adds conform.lua

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				sh = { "shfmt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			require("conform").format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
