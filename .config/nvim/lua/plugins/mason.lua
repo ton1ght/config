a33b15a314d4a0fa3690f95fba0f15209947bed4
ton1ght <ton1ght@protonmail.com>
2024-08-19 14:39:36 +0200

    nvim: format mason.lua

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,47 +1,47 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                ui = {
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗",
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = {
-            "williamboman/mason.nvim",
-        },
-        config = function()
-            require("mason-lspconfig").setup()
-        end,
-    },
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        dependencies = {
-            "williamboman/mason.nvim",
-        },
-        config = function()
-            require("mason-tool-installer").setup({
-                ensure_installed = {
-                "pyright", -- python lsp
-                "clangd", -- c/cpp lsp
-                "bashls", -- bash lsp
-                "lua_ls", -- lua lsp
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			require("mason-lspconfig").setup()
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"pyright", -- python lsp
+					"clangd", -- c/cpp lsp
+					"bashls", -- bash lsp
+					"lua_ls", -- lua lsp
 
-                "shellcheck", -- bash linter
+					"shellcheck", -- bash linter
 
-                "black", -- python formatter
-                "stylua", -- lua formatter
-                "shfmt", -- sh formatter
-                }
-            })
-        end,
-    }
+					"black", -- python formatter
+					"stylua", -- lua formatter
+					"shfmt", -- sh formatter
+				},
+			})
+		end,
+	},
 }
