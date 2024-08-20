b562897cd410c79f55440d6060c1afb15f951ead
ton1ght <ton1ght@protonmail.com>
2024-08-20 15:47:11 +0200

    nvim: use oil instead of nvim-tree

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	}, -- use if prefer nvim-web-devicons
+	config = function()
+		require("oil").setup()
+	end,
+}
