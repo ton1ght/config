a2e19d859f44f5f63334796c370e9cdd4ea62972
ton1ght <ton1ght@protonmail.com>
2024-08-22 10:45:53 +0200

    nvim: add comment.nvim plugin

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,10 @@
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+return {
+	"numToStr/Comment.nvim",
+	opts = {
+		-- add any options here
+	},
+	config = function()
+		require("Comment").setup()
+	end,
+}
