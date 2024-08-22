1e0976ee8ff171e08d407a513ad21a275ae9f84f
ton1ght <ton1ght@protonmail.com>
2024-08-22 10:43:04 +0200

    nvim: add keybinds to move line up or down

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -15,3 +15,6 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+vim.keymap.set("n", "<S-j>", "<cmd>m+<CR>", { desc = "Move line up" })
+vim.keymap.set("n", "<S-k>", "<cmd>m-2<CR>", { desc = "Move line down" })
