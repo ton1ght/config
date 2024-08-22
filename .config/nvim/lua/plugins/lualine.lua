b2081a13ffbe13cf4d56953a824bcda231ba44cb
ton1ght <ton1ght@protonmail.com>
2024-08-22 11:22:51 +0200

    nvim: remove lualine seperators and components

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,21 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup({
-            sections = {
-                lualine_b = {
-                    {
-                    'diagnostics',
-                    symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
-                    }
-                }
-            }
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_b = {
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+					},
+				},
+				lualine_x = { "filetype" },
+			},
+		})
+	end,
 }
