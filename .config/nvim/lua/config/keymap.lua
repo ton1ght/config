30cf8b78c14fd5e9b862d70942cdbe5eaba1dea5
ton1ght <ton1ght@protonmail.com>
2024-08-22 11:00:28 +0200

    nvim: add keybinds for buffer management

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -16,5 +16,11 @@ vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+-- move lines up or down
 vim.keymap.set("n", "<S-j>", "<cmd>m+<CR>", { desc = "Move line up" })
 vim.keymap.set("n", "<S-k>", "<cmd>m-2<CR>", { desc = "Move line down" })
+
+-- buffer management
+vim.keymap.set("n", "<leader>bca", "<cmd>%bd<cr>", { desc = "Buffer close all" })
+vim.keymap.set("n", "<leader>bco", "<cmd>%bd|#e|bd#<cr>", { desc = "Buffer close others" })
+vim.keymap.set("n", "<leader>bcc", "<cmd>bd<cr>", { desc = "Buffer close" })
