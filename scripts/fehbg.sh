a233d8745da9defa9e5f52177346cd75ba0029f3
ton1ght <ton1ght@protonmail.com>
2024-08-28 16:11:14 +0200

    removes old files

--- scripts/fehbg.sh
+++
@@ -1,6 +0,0 @@
-if [[ $HOST -eq "kekpc" ]]
-then
-    feh --bg-scale ~/pictures/wallpaper/catppuccin.jpg
-else
-    feh --bg-fill ~/pictures/wallpaper/catppuccin.png --bg-fill ~/pictures/wallpaper/solid-grey.jpeg
-fi
