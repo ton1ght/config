fc0ef7f61f067a4e3e4065684916da252ca0d630
ton1ght <ton1ght@protonmail.com>
2024-09-08 15:06:13 +0200

    nvim: lazy: update plugins on VimEnter
    * also disable update nofications

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -20,10 +20,16 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 	},
+
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "catppuccin-mocha" } },
+
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+
 	change_detection = { enabled = false },
 })
